b099c5e87225211ef4bb5b17ef62b5b607bff0bf
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-02-10 03:05:55 -0500

    Added panels for the results to the ClientMainWindow, also added pseudocode to the backend of client for tomorrow

--- GuessTheWords/GuessTheWords/ClientMainWindow.xaml.cs
+++ GuessTheWords/GuessTheWords/ClientMainWindow.xaml.cs
@@ -20,12 +20,13 @@ namespace GuessTheWords
         {
             InitializeComponent();
         }
-
+        
+        // JULIA's FUNCTIONS!~~~~~~~~~~~ I will write these later i dont have enough brain power rn -----------------------
         /// <summary>
         /// When the user clicks on the help button open the help / how to play box
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Control that triggered the event</param>
+        /// <param name="e">Event arguments</param>
         private void help_Click(object sender, RoutedEventArgs e)
         {
             HowToPlay helpBox = new HowToPlay();
@@ -33,9 +34,19 @@ namespace GuessTheWords
             helpBox.ShowDialog();
         }
 
+        /// <summary>
+        /// When the user submits a guess the Client should connect to the server and send the guess for validation
+        /// </summary>
+        /// <param name="sender">Control that triggered the event</param>
+        /// <param name="e">Event arguments</param>
         private void submitGuess_Click(object sender, RoutedEventArgs e)
         {
-
+            // this is where the guess connection to server will go
+            string guess = GuessTextBox.Text; // get guess from user
+            // use token from server and send guess to server for validation
+            // get server results back
+            // give user feedback based on result - update found word count / list box if the word has been found already
+            
         }
 
     }
