9916dd0e10378cca7d1a60da36eb4ff1346c1cfd
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-02-09 21:42:41 -0500

    Added skeleton to main page

---
+++ GuessTheWords/GuessTheWords/ClientMainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * FILE             : ClientMainWindow.xaml.cs
+ * PROJECT          : A02-TCPIP > Client_GuessTheWords
+ * PROGRAMMER       : Bibi Murwared, Julia Jakob
+ * FIRST VERSION    : 2026-02-16
+ * DESCRIPTION      : This file connects the main window UI to the client logic.
+ */
+
+using System.Windows;
+
+namespace GuessTheWords
+{
+    /// <summary>
+    /// Interaction logic for ClientMainWindow.xaml
+    /// </summary>
+    public partial class ClientMainWindow : Window
+    {
+        public ClientMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
